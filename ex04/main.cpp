b24ab97c5572670bd48c8dc4e0762b2df43b6f84
Jose Manuel Migoya Bussolotti <jmanuelmigoya@gmai.com>
2024-02-12 16:40:02 +0100

    fix: improve error checking

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,13 +6,14 @@
 /*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 16:22:28 by jmigoya-          #+#    #+#             */
-/*   Updated: 2024/02/08 16:22:29 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2024/02/12 16:38:14 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cstddef>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 
 int main(int argc, char *argv[])
@@ -28,11 +29,17 @@ int main(int argc, char *argv[])
 	std::string infile_name = argv[1];
 	std::string needle = argv[2];
 	std::string replacement = argv[3];
-	std::ifstream infile(infile_name);
+	std::ifstream infile(infile_name.c_str());
 
 	if (!infile.is_open())
 	{
 		std::cout << "File '" << infile_name << "' not found" << std::endl;
+
+		return (1);
+	}
+	else if (infile.peek() == EOF)
+	{
+		std::cout << "File '" << infile_name << "' is empty" << std::endl;
 
 		return (1);
 	}
@@ -42,25 +49,25 @@ int main(int argc, char *argv[])
 
 		std::string outfile_name = infile_name + ".replace";
 		std::string line_read;
-		std::ofstream outfile(outfile_name);
+		std::ofstream outfile(outfile_name.c_str());
 
 		while (infile.good())
 		{
-			std::size_t start_pos = 0;
-			std::size_t end_pos = 0;
+			std::size_t needle_start_pos = 0;
+			std::size_t needle_end_pos = 0;
 			std::getline(infile, line_read);
-			while (start_pos != std::string::npos)
+			while (needle_start_pos != std::string::npos)
 			{
-				start_pos = line_read.find(needle, end_pos);
-				if (start_pos == std::string::npos)
+				needle_start_pos = line_read.find(needle, needle_end_pos);
+				if (needle_start_pos == std::string::npos)
 				{
-					outfile << line_read.substr(end_pos, line_read.npos);
+					outfile << line_read.substr(needle_end_pos, line_read.npos);
 				}
 				else
 				{
-					outfile << line_read.substr(end_pos, start_pos);
+					outfile << line_read.substr(needle_end_pos, needle_start_pos);
 					outfile << replacement;
-					end_pos = start_pos + needle.length();
+					needle_end_pos = needle_start_pos + needle.length();
 				}
 			}
 			if (infile.good())
